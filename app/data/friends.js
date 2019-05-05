30105dfd714c4bea29ba26943e1725194460aa9c
Zach <zachary.fey@gmail.com>
2019-05-05 18:22:22 -0400

    got app working, need to get better friends matches and fix some formatting

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,13 @@
 var friends = [
     {
     name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5,1]
+    photo: "https://www2.isye.gatech.edu/people/faculty/Shabbir_Ahmed/Shabbir-Ahmed-2013-d.jpg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 },
 {
     name: 'michael',
-    photo: 'ok',
-    scores: [2,3]
+    photo: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxMjkwNTYwMDEz/michael-jackson-pepsi-commercial-raw.jpg',
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
 }
  ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,14 +7,33 @@ module.exports = (app) => {
 
     app.post('/api/friends', function(req, res) {
         console.log('posting....');
-        console.log(req.body);
-        console.log(friends);
-        friends.push(req.body);
-        console.log('added friend');
-        console.log(friends);
+        const userRes = req.body
+        let minScore = 10000
+        let matchIndex
+        for (let i in friends){
+            let score = 0;
+            // console.log(friends[i].scores)
+
+            for (let j = 0; j < 9; j++){
+                console.log('index score' + j +  Math.abs(friends[i].scores[j] - userRes.scores[j]))
+                score += Math.abs(parseInt(friends[i].scores[j]) - parseInt(userRes.scores[j]));
+                console.log('total score: ' + score);
+            }
+
+            console.log('score ' + score)
+            console.log('minscore ' + minScore);
+            if (score < minScore){
+                minScore = score;
+                console.log('new min score: '+ minScore)
+                matchIndex = i ;
+            }
+        }
 
 
-        return res.json(req.body)
+        friends.push(userRes);
+        console.log('matchIndex: ' + matchIndex)
+        console.log(friends[matchIndex])
+        return res.json(friends[matchIndex])
     })
 }
 
