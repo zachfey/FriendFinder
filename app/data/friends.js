fb165efee640acda6c758cfc4c6e06592ef36b53
Zach <zachary.fey@gmail.com>
2019-05-06 18:36:20 -0400

    got heroku working, added questions, added friend results

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,28 @@
 var friends = [
     {
-    name: "Ahmed",
-    photo: "https://www2.isye.gatech.edu/people/faculty/Shabbir_Ahmed/Shabbir-Ahmed-2013-d.jpg",
-    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    name: "John Snow",
+    photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/jon-snow-rhaegar-targaryen-1554321615.jpg?crop=0.482xw:0.962xh;0,0&resize=2742:*",
+    scores: [2, 3, 1, 1, 4, 3, 1, 5, 1, 1]
 },
 {
-    name: 'michael',
-    photo: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxMjkwNTYwMDEz/michael-jackson-pepsi-commercial-raw.jpg',
-    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    name: 'Joffrey Baratheon',
+    photo: 'https://static.tvtropes.org/pmwiki/pub/images/joffrey_baratheon.png',
+    scores: [2, 1, 1, 5, 1, 1, 5, 1, 1, 1]
+},
+{
+    name: 'Tyrion Lannister',
+    photo: 'http://assets.viewers-guide.hbo.com/small59944e8fe5822@2x.jpg',
+    scores: [5, 2, 5, 4, 3, 2, 2, 2, 2, 3]
+},
+{
+    name: 'Arya Stark',
+    photo: 'https://s3.amazonaws.com/prod-media.gameinformer.com/styles/thumbnail/s3/2019/04/30/8cacafe7/arya.jpg',
+    scores: [5, 2, 1, 2, 5, 1, 5, 1, 1, 1]
+},
+{
+    name: 'Tormund Giantsbane',
+    photo: 'https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2019/04/tormund-1_copy.jpg',
+    scores: [1, 3, 1, 2, 1, 3, 5, 5, 1, 3]
 }
  ]
 
